f6062edd744f9f42b4cd513b09a260e5f06b9245
anshuman-borah <anshuman15b@gmail.com>
2026-08-27 02:13:57 +0530

    Update api.js

--- frontend-react/src/config/api.js
+++ frontend-react/src/config/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5454";
-
+export const API_URL = "https://ansh-food-backend.onrender.com";
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
